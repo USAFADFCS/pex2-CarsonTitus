266dba3cbe030db2549a9e2a152cec849c406d3b
Carson Titus <c27carson.titus@afacademy.af.edu>
2026-03-17 16:43:10 -0600

    SRTF broken

--- CPUs.c
+++ CPUs.c
@@ -260,7 +260,7 @@ void* SRTFcpu(void* param) {
         // check value of BR before going into null, replace (what?) if arriving BR is less than what we currently have
         // insert process with lower BR then remove old -> how do I get index of current process to remove it b/c it has higher BR?
         // qShortest gets index of shortest burst but I need index of the current process. 
-        if(qShortestBR(&(svars->readyQ)) > p->burstRemaining){
+        if(qShortestBR(&(svars->readyQ)) <= p->burstRemaining){
             p->requeued = true;
             qInsert(&(svars->readyQ), p);
             qRemove(&(svars->readyQ), qShortest(&(svars->readyQ))); // how do i get the index of the CURRENT process to remove it?
